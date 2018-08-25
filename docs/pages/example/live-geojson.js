077735d41577c84a511794026bb87bd8449306e6
manyiw <manyiw@uchicago.edu>
2018-08-25 22:11:05 +0800

    Translate done 100%
    Line 2 to Line 6

--- docs/pages/example/live-geojson.js
+++ docs/pages/example/live-geojson.js
@@ -1,9 +1,9 @@
 /*---
-title: Add live realtime data
-description: Use realtime GeoJSON data streams to move a [`symbol`](/mapbox-gl-js/style-spec#layers-symbol) on your map
-tags:
-  - sources
-pathname: /mapbox-gl-js/example/live-geojson/
+标题：加入实时数据
+描述：使用实时GeoJSON信息流数据移动地图上的标记
+标签:
+  - 来源
+路径名称: /mapbox-gl-js/example/live-geojson/
 ---*/
 import Example from '../../components/example';
 import html from './live-geojson.html';
