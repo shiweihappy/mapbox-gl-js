91baab0f21824b26621f6d671052bda777b13053
manyiw <manyiw@uchicago.edu>
2018-08-25 22:09:17 +0800

    Translate done 100%
    Line 2 to Line 6

--- docs/pages/example/data-join.js
+++ docs/pages/example/data-join.js
@@ -1,9 +1,9 @@
 /*---
-title: Join local JSON data with vector tile geometries
-description: Style a choropleth by merging local JSON data with vector tile geometries
-tags:
-  - sources
-pathname: /mapbox-gl-js/example/data-join/
+标题: 载入带有矢量切片几何图形的本地JSON数据
+描述：通过合成本地JSON数据与矢量切片几何图形来设置等高线
+标签:
+  - 来源
+路径名称: /mapbox-gl-js/example/data-join/
 ---*/
 import Example from '../../components/example';
 import html from './data-join.html';
