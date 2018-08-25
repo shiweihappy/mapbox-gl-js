1a2d08a6b4b827caee65807f7ab0294760888aea
MMarking <1327701627@qq.com>
2018-08-25 16:06:17 +0800

    Translate down 100%
    line 2 to line 8

--- docs/pages/example/hillshade.js
+++ docs/pages/example/hillshade.js
@@ -1,10 +1,11 @@
 /*---
-title: Add hillshading
-description: Adds raster hillshading to a map
-tags:
-  - layers
-  - sources
-pathname: /mapbox-gl-js/example/hillshade/
+标题：添加坡面阴影
+描述：在地图上添加栅格样式的坡面阴影
+标签层级：
+    -layers
+    -sources
+路径名称：
+    /mapbox-gl-js/example/hillshade/
 ---*/
 import Example from '../../components/example';
 import html from './hillshade.html';
