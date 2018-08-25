afa0bed3efa0e0fb50447471069de6bc875e0e96
MMarking <1327701627@qq.com>
2018-08-25 15:51:11 +0800

    Translate down 100%
    line 2 to line 6

--- docs/pages/example/geojson-line.js
+++ docs/pages/example/geojson-line.js
@@ -1,9 +1,9 @@
 /*---
-title: Add a GeoJSON line
-description: Add a GeoJSON line to a map.
-tags:
-  - layers
-pathname: /mapbox-gl-js/example/geojson-line/
+标题：添加一个 GeoJSON 格式的线条
+描述：在地图上添加一个 GeoJSON 格式的线条
+标签：
+  在layers这个层级
+路径名称：/mapbox-gl-js/example/geojson-line/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-line.html';
