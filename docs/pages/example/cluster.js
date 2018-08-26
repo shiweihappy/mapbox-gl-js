f7131caea4462423696206e47b4a8c7ea152ed89
Jing <jing.liu@mapbox.com>
2018-08-26 20:44:18 +0800

    Update cluster.js

--- docs/pages/example/cluster.js
+++ docs/pages/example/cluster.js
@@ -1,6 +1,6 @@
 /*---
 title: 创建一个自定义风格的地点集群
-description: 使用Mapbox GL JS的内置功能，将地图上的点可视化为集群.
+description: 使用Mapbox GL JS的内置功能，将地图上的点可视化为集群。
 tags:
   - layers
 pathname: /mapbox-gl-js/example/cluster/
