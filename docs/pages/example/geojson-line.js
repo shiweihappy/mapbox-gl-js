b521a5f9a992b7001d4e74c79214d71b539f480d
Jing <jing.liu@mapbox.com>
2018-08-26 11:16:11 +0800

    Update geojson-line.js

--- docs/pages/example/geojson-line.js
+++ docs/pages/example/geojson-line.js
@@ -1,9 +1,9 @@
 /*---
-标题：添加一个 GeoJSON 格式的线条
-描述：在地图上添加一个 GeoJSON 格式的线条
-标签：
-  在layers这个层级
-路径名称：/mapbox-gl-js/example/geojson-line/
+- title：添加一个 GeoJSON 格式的线条
+- description：在地图上添加一个 GeoJSON 格式的线条
+- tags：
+  - layers
+- pathname：/mapbox-gl-js/example/geojson-line/
 ---*/
 import Example from '../../components/example';
 import html from './geojson-line.html';
