9533e8d9eeb80c160c76d348018fb14f709a8798
Jing <jing.liu@mapbox.com>
2018-08-26 11:32:13 +0800

    Update data-join.js

--- docs/pages/example/data-join.js
+++ docs/pages/example/data-join.js
@@ -1,9 +1,9 @@
 /*---
-标题: 载入带有矢量切片几何图形的本地JSON数据
-描述：通过合成本地JSON数据与矢量切片几何图形来设置等高线
-标签:
-  - 来源
-路径名称: /mapbox-gl-js/example/data-join/
+title: 载入带有矢量切片几何图形的本地JSON数据
+description: 通过合成本地JSON数据与矢量切片几何图形来设置等高线
+tags:
+  - sources
+pathname: /mapbox-gl-js/example/data-join/
 ---*/
 import Example from '../../components/example';
 import html from './data-join.html';
