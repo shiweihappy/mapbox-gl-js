b30a0180a8249f6e37f8517d18ee9485ffe9135b
Jing <jing.liu@mapbox.com>
2018-08-26 11:26:09 +0800

    Update line-gradient.js

--- docs/pages/example/line-gradient.js
+++ docs/pages/example/line-gradient.js
@@ -1,6 +1,6 @@
 /*---
 title: 使用公式表达式创建一条渐变线
-descroption:
+descroption:  >-
     使用在（/mapbox-gl-js/style-spec#paint-line-line-gradient）路径下的[`line-gradient`]（渐变线）颜料属性和
     能可视化一个线条的起始位置的距离表达式来创建。
 tags:
