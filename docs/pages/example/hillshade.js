a8dc2edc662b20bfe3af4de92339924c433d509f
Jing <jing.liu@mapbox.com>
2018-08-26 11:21:33 +0800

    Update hillshade.js

--- docs/pages/example/hillshade.js
+++ docs/pages/example/hillshade.js
@@ -1,10 +1,10 @@
 /*---
-标题：添加坡面阴影
-描述：在地图上添加栅格样式的坡面阴影
-标签层级：
+title: 添加坡面阴影
+description: 在地图上添加栅格样式的坡面阴影
+tags: 
     -layers
     -sources
-路径名称：
+pathname: 
     /mapbox-gl-js/example/hillshade/
 ---*/
 import Example from '../../components/example';
