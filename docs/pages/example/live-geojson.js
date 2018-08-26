930db25f6357f7ac9cc274056956549b54124f1e
Jing <jing.liu@mapbox.com>
2018-08-26 11:33:40 +0800

    Update live-geojson.js

--- docs/pages/example/live-geojson.js
+++ docs/pages/example/live-geojson.js
@@ -1,9 +1,9 @@
 /*---
-标题：加入实时数据
-描述：使用实时GeoJSON信息流数据移动地图上的标记
-标签:
-  - 来源
-路径名称: /mapbox-gl-js/example/live-geojson/
+title: 加入实时数据
+description: 使用实时GeoJSON信息流数据移动地图上的标记
+tags:
+  - sources
+pathname: /mapbox-gl-js/example/live-geojson/
 ---*/
 import Example from '../../components/example';
 import html from './live-geojson.html';
