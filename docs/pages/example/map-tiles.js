c9f688149127a49a273a5df6e2fdf06972323b2b
Jing <jing.liu@mapbox.com>
2018-08-26 11:37:48 +0800

    Update map-tiles.js

--- docs/pages/example/map-tiles.js
+++ docs/pages/example/map-tiles.js
@@ -1,9 +1,9 @@
 /*---
-标题：增加栅格切片来源
-描述：在地图中使用Mapbox栅格切片来源
-标签:
-  - 来源
-路径名称: /mapbox-gl-js/example/map-tiles/
+title: 增加栅格切片来源
+description: 在地图中使用Mapbox栅格切片来源
+tags:
+  - sources
+pathname: /mapbox-gl-js/example/map-tiles/
 ---*/
 import Example from '../../components/example';
 import html from './map-tiles.html';
