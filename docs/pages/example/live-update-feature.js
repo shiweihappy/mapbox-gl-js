0cdb40a572a72b7dddd91bfc72f969505fd344dc
Jing <jing.liu@mapbox.com>
2018-08-26 11:35:30 +0800

    Update live-update-feature.js

--- docs/pages/example/live-update-feature.js
+++ docs/pages/example/live-update-feature.js
@@ -1,9 +1,9 @@
 /*---
-标题：更新实时功能
-描述：通过更新数据改变已有地图功能
-标签:
-  - 来源
-路径名称: /mapbox-gl-js/example/live-update-feature/
+title: 实时更新功能
+description: 通过实时更新数据改变已有地图功能
+tags:
+  - sources
+pathname: /mapbox-gl-js/example/live-update-feature/
 ---*/
 import Example from '../../components/example';
 import html from './live-update-feature.html';
