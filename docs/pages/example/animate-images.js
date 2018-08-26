4ca3ed30b0fc1a4a9523cbc5db507e0fe15a114f
Jing <jing.liu@mapbox.com>
2018-08-26 20:42:36 +0800

    Update animate-images.js

--- docs/pages/example/animate-images.js
+++ docs/pages/example/animate-images.js
@@ -1,6 +1,6 @@
 /*---
 title: 动态显示一系列的地图图像
-description: 使用一系列[图片资源](/mapbox-gl-js/style-spec#sources-image) 来制作一个动态地图动画. 
+description: 使用一系列[图片资源](/mapbox-gl-js/style-spec#sources-image) 来制作一个动态地图动画。
 tags:
   - layers
   - sources
