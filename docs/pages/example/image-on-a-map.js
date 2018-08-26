29a66a686cf7ef9d7b679e9d195b4f092a2d3818
Jing <jing.liu@mapbox.com>
2018-08-26 11:23:34 +0800

    Update image-on-a-map.js

--- docs/pages/example/image-on-a-map.js
+++ docs/pages/example/image-on-a-map.js
@@ -1,10 +1,10 @@
 /*---
-标题：添加一张纹理图片
-描述：基线矢量暗纹理与天气影像图叠加
-标签：
+title: 添加一张纹理图片
+description: 基线矢量暗纹理与天气影像图叠加
+tags:
   - sources
   - layers
-文件路径：/mapbox-gl-js/example/image-on-a-map/
+pathname: /mapbox-gl-js/example/image-on-a-map/
 ---*/
 import Example from '../../components/example';
 import html from './image-on-a-map.html';
