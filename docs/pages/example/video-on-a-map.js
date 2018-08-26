8955811341524382520ab8043c41e8a1df281341
Jing <jing.liu@mapbox.com>
2018-08-26 11:30:58 +0800

    Update video-on-a-map.js

--- docs/pages/example/video-on-a-map.js
+++ docs/pages/example/video-on-a-map.js
@@ -1,10 +1,10 @@
 /*---
-标题：加入视频
-描述：带有视频的卫星栅格图层
-标签:
-  - 来源
-  - 图层
-路径名称: /mapbox-gl-js/example/video-on-a-map/
+title: 加入视频
+description: 带有视频的卫星栅格图层
+tags:
+  - sources
+  - layers
+pathname: /mapbox-gl-js/example/video-on-a-map/
 ---*/
 import Example from '../../components/example';
 import html from './video-on-a-map.html';
